db9ca965c97b384cbdc671390fec8b84f501b8ef
Paul Cassidy <ptc6340@rit.edu>
2012-02-08 00:43:24 -0500

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinkyIn::Application.config.secret_token = '849b3bfc9e9cb62b81146ad3055226bde11303374aa09dbc2af353652f62422e8b16a507fc873b974f1cb4030a97254654dd05906d968c6e2c49c7c0483e4758'
